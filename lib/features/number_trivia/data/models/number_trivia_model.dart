1da3a47411b72fef835c0f24bb27e1a6c16b0b34
Ryo WATANABE <30409123+nabe90s@users.noreply.github.com>
2021-06-29 00:37:09 +0900

    create test, fromJson, toJson

--- lib/features/number_trivia/data/models/number_trivia_model.dart
+++ lib/features/number_trivia/data/models/number_trivia_model.dart
@@ -5,4 +5,16 @@ class NumberTriviaModel extends NumberTrivia {
     required String text,
     required int number,
   }) : super(text: text, number: number);
+
+  Map<String, dynamic> toJson() {
+    return {
+      'text': text,
+      'number': number,
+    };
+  }
+
+  factory NumberTriviaModel.fromJson(Map<String, dynamic> json) {
+    return NumberTriviaModel(
+        text: json['text'], number: (json['number'] as num).toInt());
+  }
 }
