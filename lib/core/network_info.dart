2d5a98807972bd0ba87372a643c968bac3dd0b43
Ryo WATANABE <30409123+nabe90s@users.noreply.github.com>
2021-06-30 10:25:57 +0900

    update network_info.dart add connectivity_plus

--- lib/core/network_info.dart
+++ lib/core/network_info.dart
@@ -1,3 +1,28 @@
+import 'package:connectivity_plus/connectivity_plus.dart';
+
 abstract class NetworkInfo {
   Future<bool> get isConnected;
+}
+
+class NetworkInfoImpl implements NetworkInfo {
+  final Connectivity connectivity;
+
+  NetworkInfoImpl({
+    required this.connectivity,
+  });
+
+  @override
+  Future<bool> get isConnected => _isConnected();
+
+  Future<bool> _isConnected() async {
+    var connectivityResult = await (connectivity.checkConnectivity());
+    if (connectivityResult == ConnectivityResult.mobile) {
+      // I am connected to a mobile network.
+      return true;
+    } else if (connectivityResult == ConnectivityResult.wifi) {
+      // I am connected to a wifi network.
+      return true;
+    }
+    return false;
+  }
 }
