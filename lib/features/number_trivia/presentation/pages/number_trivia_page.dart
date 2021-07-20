fa834ed87307d8d660446f2c2cf50b3a35be17eb
Ryo WATANABE <30409123+nabe90s@users.noreply.github.com>
2021-07-20 10:04:54 +0900

    create number_trivia_page.dart

--- lib/features/number_trivia/presentation/pages/number_trivia_page.dart
+++ lib/features/number_trivia/presentation/pages/number_trivia_page.dart
@@ -1,38 +1,48 @@
+import 'package:clean_architecture_tdd/features/number_trivia/presentation/bloc/number_trivia/number_trivia_bloc.dart';
+import 'package:clean_architecture_tdd/injection_container.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import '../bloc/number_trivia/number_trivia_bloc.dart';
 
-// Uncomment if you use injector package.
-// import 'package:my_app/framework/di/injector.dart';
-
-class NumberTrivia extends StatefulWidget {
-
-  @override
-  State<StatefulWidget> createState() {
-    return NumberTriviaState();
-  }
-}
-
-class NumberTriviaState extends State<NumberTrivia> {
-
-  // Insert into injector file if you use it.
-  // injector.map<NumberTriviaBloc>((i) => NumberTriviaBloc(i.get<Repository>()), isSingleton: true);
-
-  // Uncomment if you use injector.
-  // final _bloc = injector.get<NumberTriviaBloc>();
-
-  @override
-  void initState() {
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-  }
+class NumberTriviaPage extends StatelessWidget {
+  const NumberTriviaPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Number Trivia'),
+      ),
+      body: BlocProvider<NumberTriviaBloc>(
+        create: (context) => sl<NumberTriviaBloc>(),
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(10),
+            child: Column(
+              children: <Widget>[
+                SizedBox(height: 10),
+                Container(
+                  height: MediaQuery.of(context).size.height / 3,
+                  child: Placeholder(),
+                ),
+                SizedBox(height: 10),
+                Column(
+                  children: <Widget>[
+                    Placeholder(fallbackHeight: 40),
+                    SizedBox(height: 10),
+                    Row(
+                      children: <Widget>[
+                        Expanded(child: Placeholder(fallbackHeight: 30)),
+                        SizedBox(width: 10),
+                        Expanded(child: Placeholder(fallbackHeight: 30)),
+                      ],
+                    )
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
